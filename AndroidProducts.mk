98c246c27e00aed0bcb9c04bcaf5a5dbdac8180a
Maik Stratemeyer <maik@stratemeyer.tk>
2020-02-11 19:34:24 +0100

    Add Xtended Support

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_cedric.mk
+    $(LOCAL_DIR)/xtended_cedric.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_cedric-user \
-    aosp_cedric-userdebug \
-    aosp_cedric-eng
+    xtended_cedric-user \
+    xtended_cedric-userdebug \
+    xtended_cedric-eng

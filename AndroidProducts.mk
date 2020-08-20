bda4dd5a16cdafc3e28e69b7db8379224878a50d
kiam001 <blablatupfen@gmail.com>
2020-08-20 19:35:53 +0200

    Add Support fot bliss ROM

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_cedric.mk
+    $(LOCAL_DIR)/bliss_cedric.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_cedric-user \
-    aosp_cedric-userdebug \
-    aosp_cedric-eng
+    bliss_cedric-user \
+    bliss_cedric-userdebug \
+    bliss_cedric-eng
